14fb2a0d1a516c10e7dafca3bd89b2bddc1016bb
jystella17 <42269221+jystella17@users.noreply.github.com>
2022-01-12 22:15:03 +0900

    Update settings.py

--- hp_server/settings.py
+++ hp_server/settings.py
@@ -24,6 +24,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = secrets['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
